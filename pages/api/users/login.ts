8df9650e7aa155350af5b4ef35453a7b8fc3f6eb
jinyoung <danroom85@gmail.com>
2022-07-12 00:46:16 +0900

    Mod handler in login+signup

--- pages/api/users/login.ts
+++ pages/api/users/login.ts
@@ -1,7 +1,6 @@
 import withHandler, { ResponseType }  from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
-import { withApiSession } from "@libs/server/withSession";
 
 async function handler(
     req:NextApiRequest, res:NextApiResponse<ResponseType>
